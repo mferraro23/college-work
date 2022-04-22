6f786e2b33628abcda420f97dd7d0ec0a1609f19
Michael Ferraro <mf69386p@pace.edu>
2022-04-21 20:22:37 -0400

    updates cw

--- Classwork/CWApr13/ferraromapril13.js
+++ Classwork/CWApr13/ferraromapril13.js
@@ -1,8 +1,8 @@
 function myPassword(numChar){
-	myArray = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
-	var password="";
-	for (var i=0; i<numChar; i++){
-		var randomNum = Math.floor(Math.random()*26);
+	let myArray = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+	let password = "";
+	for (let i=0; i<numChar; i++){
+		const randomNum = Math.floor(Math.random() * 26);
 		password += myArray[randomNum];
 
 	}
